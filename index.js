453a6646c5c3dc89162c1738943baadc1fc8aff6
Gemeng <qingemeng@gmail.com>
2017-09-16 21:50:55 +0800

    Setup yarn, eslint, babel

--- index.js
+++ index.js
@@ -1,79 +1,80 @@
-"use strict";
+'use strict';
 
-const Telegraf = require('telegraf')
-const TelegrafFlow = require('telegraf-flow')
-const db = require('./db')
+import Telegraf from 'telegraf';
+import TelegrafFlow from 'telegraf-flow';
 
-const {Extra} = Telegraf
+const db = require('./db');
 
-const {WizardScene} = TelegrafFlow
-const flow = new TelegrafFlow()
+const {Extra} = Telegraf;
 
-flow.command('add_group', context => context.flow.enter('addGroup'))
+const {WizardScene} = TelegrafFlow;
+const flow = new TelegrafFlow();
 
-let name, link
+flow.command('add_group', context => context.flow.enter('addGroup'));
+
+let name, link;
 const addGroupScene = new WizardScene('addGroup',
     context => {
         db.doorCheck(context)
             .then((passed) => {
                 if (!passed) {
-                    return context.reply("you haven't key in the magic words")
+                    return context.reply('you haven\'t key in the magic words');
                 }
-                context.reply("What's the group name?")
-                context.flow.wizard.next()
-            })
+                context.reply('What\'s the group name?');
+                context.flow.wizard.next();
+            });
     },
     context => {
-        name = context.message.text
-        context.reply("What's the group invitation link (you have to be the creator)?")
-        context.flow.wizard.next()
+        name = context.message.text;
+        context.reply('What\'s the group invitation link (you have to be the creator)?');
+        context.flow.wizard.next();
     },
     context => {
-        link = context.message.text
-        context.reply(`Done, adding ${name}, ${link}`)
-        db.write('groups/', name, link)
-        context.flow.leave()
-    })
+        link = context.message.text;
+        context.reply(`Done, adding ${name}, ${link}`);
+        db.write('groups/', name, link);
+        context.flow.leave();
+    });
 
-flow.register(addGroupScene)
+flow.register(addGroupScene);
 
 flow.command('start', context => {
-    context.flow.enter('authenticate')
-    console.log('start...')
-})
+    context.flow.enter('authenticate');
+    console.log('start...');
+});
 const authenticateScene = new WizardScene('authenticate',
     context => {
-        context.reply("What's the magic words?")
-        context.flow.wizard.next()
+        context.reply('What\'s the magic words?');
+        context.flow.wizard.next();
     },
     context => {
         db.auth(context)
             .then((passed) => {
-                return context.reply(passed ? 'Welcome' : 'Wrong password')
-            })
-        console.log(Telegraf.memorySession())
-        context.flow.leave()
-    })
+                return context.reply(passed ? 'Welcome' : 'Wrong password');
+            });
+        console.log(Telegraf.memorySession());
+        context.flow.leave();
+    });
 
-flow.register(authenticateScene)
+flow.register(authenticateScene);
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.use(Telegraf.memorySession())
-bot.on('text', flow.middleware())
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.use(Telegraf.memorySession());
+bot.on('text', flow.middleware());
 
 bot.command('list_groups', (context) => {
     db.doorCheck(context)
         .then((passed) => {
             if (!passed) {
-                return context.reply("you haven't key in the magic words")
+                return context.reply('you haven\'t key in the magic words');
             }
 
             db.read('groups/').then(function (groupsMapping) {
-                const groups = Object.entries(groupsMapping).map(([name, link]) => `- [${name}](${link})`)
-                return context.reply(groups.join('\n'), Extra.markdown())
-            })
-        })
-})
+                const groups = Object.entries(groupsMapping).map(([name, link]) => `- [${name}](${link})`);
+                return context.reply(groups.join('\n'), Extra.markdown());
+            });
+        });
+});
 
-console.log('Starting the bot…')
-bot.startPolling()
+console.log('Starting the bot…');
+bot.startPolling();
